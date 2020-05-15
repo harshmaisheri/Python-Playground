519139f46294def4e0edde4c22795b8afd5d7c8b
harshmaisheri <harshmaisheri@gmail.com>
2020-05-15 12:12:30 +0530

    feat: EmailSender

---
+++ venv/EmailSender/email_sender.py
@@ -0,0 +1,18 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text)
+email = EmailMessage()
+email['from'] = "Harsh Maisheri"
+email['to'] = 'harshmaisheri@gmail.com'
+email['subject'] = "Wallah~!! Tested Ok"
+
+email.set_content(html.substitute({"name": "Mojo Jojo"}), html)
+
+with smtplib.SMTP(host="smtp.gmail.com", port=587)as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('shah.harsh143@gmail.com', 'lovehurts123')
+    smtp.send_message(email)
