35cc53c77cd5f1cba908e3a4a609afd6b8e17adf
harshmaisheri <harshmaisheri@gmail.com>
2020-05-15 12:36:56 +0530

    refact: Files

--- venv/sms.py
+++ venv/sms.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC654fb2b954a9b797a063c5210d5a4c50'
-auth_token = '35c6be1371c069a40af2234fdd5c6859'
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
 
 message = client.messages \
